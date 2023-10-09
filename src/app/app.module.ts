df7057f68389b3cbaf21c0aff98570d5132e1f5d
d-anthero <d.anthero09@gmail.com>
2023-10-09 16:27:45 -0300

    update ps-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    MenuBarComponent,
+    HomeComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
